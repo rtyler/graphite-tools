e4ed5730ad7f8a243fb9af95a3fcfd579d6b8b95
R. Tyler Croy <tyler@monkeypox.org>
2011-03-25 17:31:33 -0700

    Add a basic load monitor script to print out graphite formatted metrics

---
+++ monitors/load.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import optparse
+import os
+import platform
+import sys
+import time
+
+def main():
+    proc_path = '/proc/loadavg'
+
+    if not os.path.exists(proc_path):
+        sys.stderr.write('load.py only supports systems with /proc\n')
+        return 1
+
+    opts = optparse.OptionParser()
+    opts.add_option('-p', '--prefix', dest='prefix',
+                help='Specify a prefix for the event labels (e.g. foo.system.load)')
+    options, args = opts.parse_args()
+
+    pieces = None
+
+    with open(proc_path, 'r') as fd:
+        buf = fd.read().strip()
+        pieces = buf.split(' ')
+
+    if not pieces or len(pieces) < 3:
+        return 1
+
+    one, five, fifteen = pieces[0], pieces[1], pieces[2]
+    now = int(time.time())
+    prefix = ''
+
+    if options.prefix:
+        prefix = '%s.' % options.prefix
+
+    print '%(prefix)ssystem.load.1min %(one)s %(now)s' % locals()
+    print '%(prefix)ssystem.load.5min %(five)s %(now)s' % locals()
+    print '%(prefix)ssystem.load.15min %(fifteen)s %(now)s' % locals()
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
+
